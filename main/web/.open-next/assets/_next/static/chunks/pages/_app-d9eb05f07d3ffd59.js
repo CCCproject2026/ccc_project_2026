3274e2f8207b533ca1355bdca54d015cea55a58d
Riddler <winhtutooheart2@gmail.com>
2026-06-16 19:59:26 +0900

    tesing deploy

---
+++ main/web/.open-next/assets/_next/static/chunks/pages/_app-d9eb05f07d3ffd59.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{7518:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(7081)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(7518),n(6285))),_N_E=_.O()}]);
