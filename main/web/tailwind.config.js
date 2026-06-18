882817c8036929a6e848964855672835208b85d6
khalarlay21501 <C250324@ccg.ac.jp>
2026-06-19 01:51:23 +0900

    ok

--- main/web/tailwind.config.js
+++ main/web/tailwind.config.js
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-=======
-	content: ["./src/**/*.{ts,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
->>>>>>> main
 };
